37d5d0bbf6e92d7329ee6a0e6955becfa4bce853
chn <chn@chn.moe>
2025-03-17 14:12:27 +0800

    删除一些人

--- Form1.vb
+++ Form1.vb
@@ -10,18 +10,17 @@
         },
         New List(Of String) From
         {
-            "刘辉强", "吐尔洪江·吾拉木", "宿一曼", "封辉", "张思晴", "张润涵", "李昊天", "杜颖", "杨仁琪", "林梓淇",
-            "林高翔", "梁雁彤", "王小丹", "翁小蒙", "莫仕弟", "萨娜", "邓铁龙", "陈子涵", "陈豪飞", "马修益",
-            "鲁浩伟"
+            "刘辉强", "宿一曼", "封辉", "张思晴", "张润涵", "李昊天", "杨仁琪", "林梓淇",
+            "林高翔", "梁雁彤", "翁小蒙", "莫仕弟", "陈子涵", "陈豪飞", "马修益"
         },
         New List(Of String) From
         {
             "任思豪", "刘世林", "刘启霖", "刘敏", "刘文博", "刘源 ", "刘金华", "危永平", "叶晓芳", "叶颖",
-            "吴世明", "周博语", "周江鹏", "姚文龙", "宋欣格", "常青", "廖桂敏", "张为智", "张姝卿", "张宗南",
+            "吴世明", "周博语", "周江鹏", "宋欣格", "常青", "廖桂敏", "张姝卿", "张宗南",
             "张文琴", "张晨昊", "张灵辉", "张耀宗", "张艺腾", "张裕祥", "曹宇", "曹硕", "曾国清", "李彦君",
             "李文翔", "李欣如", "李洪树", "杨镇泽", "林旺康", "柳梦宇", "王必成", "王曼", "王颖萌", "程可扬",
             "程安琪", "程舒仪", "莫炳杰", "蒋中升", "蓝进肾", "詹永萍", "赖麒鸿", "赵志国", "郑一帆", "郭叁育",
-            "陈垚", "陈子龙", "陈玮健", "陈紫怡", "陈阳", "马梦薇", "高扬钧", "黄健", "黄金理", "龚彬"
+            "陈垚", "陈玮健", "陈紫怡", "陈阳", "马梦薇", "高扬钧", "黄健", "龚彬"
         }
     }
     Private StudentsUsed_ As New List(Of List(Of String)) From {New List(Of String), New List(Of String), New List(Of String)}
@@ -110,15 +109,6 @@
                     TextBox_(0).Text = "杨嘉乐"
                 Else
                     TextBox_(0).Text = "杨鑫雨"
-                End If
-                TextBox_(0).Refresh()
-            ElseIf TextBox_(0).Text.Length = 3 AndAlso TextBox_(0).Text(0) = "项" AndAlso TextBox_(0).Text(1) = TextBox_(0).Text(2) Then
-                If (New Random).Next(0, 4) = 0 Then
-                    Dim alternate_chars As New List(Of Char) From {CChar("鑫"), CChar("森"), CChar("淼"), CChar("焱"), CChar("垚")}
-                    Dim alternate_char = alternate_chars((New Random).Next(0, alternate_chars.Count))
-                    TextBox_(0).Text = "项" + alternate_char + alternate_char
-                Else
-                    TextBox_(0).Text = "项磊磊"
                 End If
                 TextBox_(0).Refresh()
             End If
